c0b1c42bfeac959b0e9007d89a908f476123367c
Wallace Costa <wcosta@unifesp.br>
2025-03-03 19:55:58 -0300

    Executando testes iniciais

--- Program.cs
+++ Program.cs
@@ -5,6 +5,25 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Iniciando teste");
+//Falta arrumar o DAQmx para versao aceita pelo codigo
+        /*using(NationalInstruments.DAQmx.Task Tarefa = new NationalInstruments.DAQmx.Task()){
+            Tarefa.AIChannels.CreateVoltageChannel(
+            "Dev1/ai0",
+            "CanalSimulado",
+            AITerminalConfiguration.Differential,
+            0,10,
+            AIVoltageUnits.Volts);
+            AnalogSingleChannelReader leitor = new AnalogSingleChannelReader(Tarefa.Stream);
+            Tarefa.Start();
+
+            for(int i = 0;i<10;i++){
+                double valorLido = leitor.ReadSingleSample();
+                Console.WriteLine($"Leitura{i+1}:{valorLido:F3} V");
+                System.Threading.Thread.Sleep(500);
+            }
+            Tarefa.Stop();
+        }*/
+        Console.WriteLine("Finalizado");
     }
 }
--- obj/Debug/net8.0/ProjetoTeste.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjetoTeste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoTeste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a32e946a8c35b6b4b12148600da2b0d7a97b1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoTeste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoTeste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
